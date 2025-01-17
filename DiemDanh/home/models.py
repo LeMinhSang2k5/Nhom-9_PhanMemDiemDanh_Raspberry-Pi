db48725fdea37a5508f90177bb094b1fc138816d
Phat <phatkt0899@ut.edu.vn>
2025-01-18 00:54:06 +0700

    update admin.py models.py urls.py views.py

--- DiemDanh/home/models.py
+++ DiemDanh/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=255)
+    gender = models.BooleanField()
+    birth_day = models.DateField()
+    id = models.IntegerField(_("Student ID"), primary_key=True)
+    email = models.EmailField(_("Email Address"), unique=True)
