f2ac4c16c02238093d7aae12a5d65a6321c62d20
Phat <phatkt0899@ut.edu.vn>
2025-01-19 00:21:16 +0700

    update databse

--- DiemDanh/home/admin.py
+++ DiemDanh/home/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import student
+from .models import student,teacher,principal
 # Register your models here.
 admin.site.register(student)
+admin.site.register(teacher)
+admin.site.register(principal)
--- DiemDanh/home/models.py
+++ DiemDanh/home/models.py
@@ -4,9 +4,29 @@ from django.db import models
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+#Database hs
 class student(models.Model):
     name = models.CharField(max_length=255)
     gender = models.BooleanField()
-    birth_day = models.DateField()
+    birth_day = models.DateField(null=True, blank=True)
     id = models.IntegerField(_("Student ID"), primary_key=True)
+    email = models.EmailField(_("Email Address"), unique=True)
+    
+
+    #Database gv
+class teacher(models.Model):
+    name_teacher = models.CharField(max_length=255, null=True)
+    gender = models.BooleanField()
+    birth_day = models.DateField(null=True, blank=True)
+    id = models.IntegerField(_("Teacher ID"), primary_key=True)
+    email = models.EmailField(_("Email Address"), unique=True)
+
+
+class principal(models.Model):
+    name_principal = models.CharField(max_length=255, null=True)
+    role = models.CharField(max_length=255, default="Default Role")
+    gender = models.BooleanField()
+    birth_day = models.DateField(null=True, blank=True)
+    id = models.IntegerField(_("Principal ID"), primary_key=True)
     email = models.EmailField(_("Email Address"), unique=True)
